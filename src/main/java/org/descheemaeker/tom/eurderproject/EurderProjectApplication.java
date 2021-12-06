5e60edd56b11ff6e57447e07f9e751ddeea303e2
Tom Descheemaeker <tomdescheemaeker@hotmail.com>
2021-12-06 11:43:09 +0100

    Springboot Initializer setup

---
+++ src/main/java/org/descheemaeker/tom/eurderproject/EurderProjectApplication.java
@@ -0,0 +1,13 @@
+package org.descheemaeker.tom.eurderproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurderProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurderProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/descheemaeker/tom/eurderproject/EurderProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.descheemaeker.tom.eurderproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurderProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
