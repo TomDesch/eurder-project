e43001543bd666931f1469e76e5cac74592c9588
Tom Descheemaeker <tomdescheemaeker@hotmail.com>
2021-12-06 15:00:37 +0100

    Build: Corrected Test classpath

--- src/test/java/org/descheemaeker/tom/eurderproject/EurderProjectApplicationTests.java
+++ src/test/java/org/descheemaeker/tom/eurderproject/EurderProjectApplicationTests.java
@@ -1,13 +1,13 @@
 package org.descheemaeker.tom.eurderproject;
 
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EurderProjectApplicationTests {
 
 	@Test
-	void contextLoads() {
+	 void contextLoads() {
 	}
 
 }
