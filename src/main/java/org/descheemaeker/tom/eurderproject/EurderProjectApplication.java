9a4dd14371247448ae7a5ad0945c895eb71fd1d8
Tom Descheemaeker <tomdescheemaeker@hotmail.com>
2021-12-06 14:34:03 +0100

    Build: Updated pom.xml

--- src/main/java/org/descheemaeker/tom/eurderproject/EurderProjectApplication.java
+++ src/main/java/org/descheemaeker/tom/eurderproject/EurderProjectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EurderProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurderProjectApplication.class, args);
+		SpringApplication.run(EurderProjectApplication.class);
 	}
 
 }
