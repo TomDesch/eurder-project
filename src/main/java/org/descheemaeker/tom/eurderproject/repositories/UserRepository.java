10d11fdccd8d8d30149676fb4e64b1cf585e12c5
Tom Descheemaeker <tomdescheemaeker@hotmail.com>
2022-01-03 11:03:38 +0100

    feat(self eval): Updated all of UserRepository.java to use db except getUser

--- src/main/java/org/descheemaeker/tom/eurderproject/repositories/UserRepository.java
+++ src/main/java/org/descheemaeker/tom/eurderproject/repositories/UserRepository.java
@@ -3,27 +3,32 @@ package org.descheemaeker.tom.eurderproject.repositories;
 import org.descheemaeker.tom.eurderproject.domain.User;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
 public class UserRepository {
-    private final Map<String, User> users = new ConcurrentHashMap<>();
 
-    public User addUser(User user) {
-        users.put(user.getUserId(), user);
-        return user;
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
+    public void addUser(User user) {
+        entityManager.persist(user);
     }
 
-    public User getUser(String userId) {
-        return users.get(userId);
-    }
 
     public List<User> getAllUsers() {
-        return new ArrayList<>(users.values());
+        return entityManager.createQuery("select u from User u", User.class)
+                .getResultList();
     }
+
+
+//    public User getUser(String userId) {
+//        return users.get(userId);
+//    }
 
 
 }
