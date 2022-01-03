82607b763cd008125f55774beeb992883dd4f793
Tom Descheemaeker <tomdescheemaeker@hotmail.com>
2022-01-03 12:51:09 +0100

    fix(self eval): Updated intellIJ to know about records...

--- src/main/java/org/descheemaeker/tom/eurderproject/api/items/dto/CreateItemDto.java
+++ src/main/java/org/descheemaeker/tom/eurderproject/api/items/dto/CreateItemDto.java
@@ -1,4 +1,4 @@
 package org.descheemaeker.tom.eurderproject.api.items.dto;
 
-public record CreateItemDto(String name, String description, double price, int amount) {
-}
+public record CreateItemDto(String name,String description,double price,int amount){
+        }
