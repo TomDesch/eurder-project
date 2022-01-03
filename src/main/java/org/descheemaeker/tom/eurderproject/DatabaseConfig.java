86319ca4f8012a8750988732b2304205a8944e0d
Tom Descheemaeker <tomdescheemaeker@hotmail.com>
2022-01-03 10:33:22 +0100

    feat(self eval): Added DatabaseConfig.java and updated pom accordingly

---
+++ src/main/java/org/descheemaeker/tom/eurderproject/DatabaseConfig.java
@@ -0,0 +1,19 @@
+package org.descheemaeker.tom.eurderproject;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+public class DatabaseConfig {
+
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+}
